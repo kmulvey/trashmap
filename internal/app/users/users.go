76827197f49134fc1c655e4b5bb94ac61444c7cd
Kevin Mulvey <kmulvey@linux.com>
2022-04-15 17:21:43 -0600

    update sigs

--- internal/app/users/users.go
+++ internal/app/users/users.go
@@ -3,11 +3,14 @@ package trashapp
 import (
 	"fmt"
 	"net/mail"
+
+	"github.com/kmulvey/trashmap/internal/app/config"
+	"github.com/kmulvey/trashmap/internal/app/db"
 )
 
-func AddUser(config *Config, email string, contactAllowed bool) error {
+func AddUser(config *config.Config, email string, contactAllowed bool) error {
 	if _, err := mail.ParseAddress(email); err != nil {
-		return InsertUser(config.DBConn, email, contactAllowed)
+		return db.InsertUser(config.DBConn, email, contactAllowed)
 	}
 	return fmt.Errorf("%-30s is not a valid email", email)
 }
