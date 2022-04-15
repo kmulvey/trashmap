f1b6ab38c7e72d05523dcd8450411a6ed35b668a
Kevin Mulvey <kmulvey@linux.com>
2022-04-15 16:04:40 -0600

    auth table

--- sql/create.sql
+++ sql/create.sql
@@ -1,10 +1,18 @@
 DROP TABLE areas;
+DROP TABLE auth;
 DROP TABLE users;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(128) NOT NULL,
     contact_allowed boolean DEFAULT false
+);
+
+CREATE TABLE auth (
+    auth_token varchar(32) PRIMARY KEY,
+    user_id SERIAL,
+	created timestamp NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE areas (
