20730fe8b2b989858fc9d25f681ff993aafce90f
Kevin Mulvey <kmulvey@linux.com>
2022-04-15 21:57:15 -0600

    poly routes

--- internal/app/webserver/routes.go
+++ internal/app/webserver/routes.go
@@ -2,45 +2,16 @@ package webserver
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kmulvey/trashmap/internal/app/config"
-	"github.com/kmulvey/trashmap/internal/app/users"
+	"github.com/kmulvey/trashmap/internal/app/polygon"
 )
 
-func CreateUser(config *config.Config, c *gin.Context) {
-	var email = c.PostForm("email")
-	var contactAllowedStr = c.PostForm("contactAllowed")
-	var contactAllowed, err = strconv.ParseBool(contactAllowedStr)
-	if err != nil {
-		c.JSON(
-			http.StatusBadRequest,
-			gin.H{
-				"error":     "unable to ParseBool: " + contactAllowedStr,
-				"raw_error": err.Error(),
-			},
-		)
-		return
-	}
+func CreatePolygon(config *config.Config, c *gin.Context) {
+	var polygonStr = c.PostForm("polygon")
 
-	err = users.AddUser(config, email, contactAllowed)
-	if err != nil {
-		c.JSON(
-			http.StatusInternalServerError,
-			gin.H{
-				"error":     "unable to add user",
-				"raw_error": err.Error(),
-			},
-		)
-		return
-	}
-	c.Status(http.StatusOK)
-}
-
-func DeleteUser(config *config.Config, c *gin.Context) {
-	var email = c.PostForm("email")
-	var err = users.RemoveUser(config, email)
+	var err = polygon.SavePolygon(config, 0, polygonStr)
 	if err != nil {
 		c.JSON(
 			http.StatusInternalServerError,
---
+++ internal/app/webserver/user_routes.go
@@ -0,0 +1,55 @@
+package webserver
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kmulvey/trashmap/internal/app/config"
+	"github.com/kmulvey/trashmap/internal/app/users"
+)
+
+func CreateUser(config *config.Config, c *gin.Context) {
+	var email = c.PostForm("email")
+	var contactAllowedStr = c.PostForm("contactAllowed")
+	var contactAllowed, err = strconv.ParseBool(contactAllowedStr)
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"error":     "unable to ParseBool: " + contactAllowedStr,
+				"raw_error": err.Error(),
+			},
+		)
+		return
+	}
+
+	err = users.AddUser(config, email, contactAllowed)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"error":     "unable to add user",
+				"raw_error": err.Error(),
+			},
+		)
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func DeleteUser(config *config.Config, c *gin.Context) {
+	var email = c.PostForm("email")
+	var err = users.RemoveUser(config, email)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"error":     "unable to remove user",
+				"raw_error": err.Error(),
+			},
+		)
+		return
+	}
+	c.Status(http.StatusOK)
+}
