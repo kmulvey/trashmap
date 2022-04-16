310be029b1c16d65298c4c46fcf83c08b3554ca3
Kevin Mulvey <kmulvey@linux.com>
2022-04-15 21:58:11 -0600

    poly app

---
+++ internal/app/polygon/polygon.go
@@ -0,0 +1,28 @@
+package polygon
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/kmulvey/trashmap/internal/app/config"
+	"github.com/kmulvey/trashmap/internal/app/db"
+)
+
+func SavePolygon(config *config.Config, userID int, polygonStr string) error {
+	var polygonArr = strings.Split(polygonStr, ",")
+
+	// first check that we got enough data
+	if len(polygonArr)%2 != 0 {
+		return errors.New("gps points are malformed (lenght is odd, should be even given they are pairs")
+	}
+
+	// chech that the things in between the commas are actually floats
+	for _, point := range polygonArr {
+		if _, err := strconv.ParseFloat(point, 64); err != nil {
+			return err
+		}
+	}
+
+	return db.InsertArea(config.DBConn, userID, polygonStr)
+}
