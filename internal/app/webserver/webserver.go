c19445cbd1d8358ba19b0f1f58013946b1c68658
Kevin Mulvey <kmulvey@linux.com>
2022-04-15 21:57:29 -0600

    more http stuff

--- internal/app/webserver/webserver.go
+++ internal/app/webserver/webserver.go
@@ -1,6 +1,11 @@
 package webserver
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-contrib/httpsign"
+	"github.com/gin-contrib/httpsign/crypto"
+	"github.com/gin-contrib/secure"
 	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	"github.com/kmulvey/trashmap/internal/app/config"
@@ -8,13 +13,61 @@ import (
 )
 
 func StartWebServer(config *config.Config, runLocal bool) {
-	r := gin.Default()
-	r.PUT("/user", func(c *gin.Context) { CreateUser(config, c) })
-	r.DELETE("/user", func(c *gin.Context) { DeleteUser(config, c) })
+	var router = gin.Default()
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+
+	// CORS
+	if runLocal {
+		router.Use(cors.Default())
+	} else {
+		var corsConfig = cors.DefaultConfig()
+		corsConfig.AllowOrigins = []string{config.HTTPAddr}
+		router.Use(cors.New(corsConfig))
+	}
+
+	// compress
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	// secure things a bit
+	router.Use(secure.New(secure.Config{
+		AllowedHosts:          []string{config.HTTPAddr},
+		SSLRedirect:           true,
+		STSSeconds:            315360000,
+		STSIncludeSubdomains:  true,
+		FrameDeny:             true,
+		ContentTypeNosniff:    true,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self'",
+		IENoOpen:              true,
+		ReferrerPolicy:        "strict-origin-when-cross-origin",
+		SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
+	}))
+
+	// HTTP signing
+	var readKeyID = httpsign.KeyID("read")
+	var writeKeyID = httpsign.KeyID("write")
+	var secrets = httpsign.Secrets{
+		readKeyID: &httpsign.Secret{
+			Key:       config.HTTPReadSigningSecret,
+			Algorithm: &crypto.HmacSha512{},
+		},
+		writeKeyID: &httpsign.Secret{
+			Key:       config.HTTPWriteSigningSecret,
+			Algorithm: &crypto.HmacSha512{},
+		},
+	}
+	auth := httpsign.NewAuthenticator(secrets)
+	router.Use(auth.Authenticated())
+
+	// routes
+	router.PUT("/user", func(c *gin.Context) { CreateUser(config, c) })
+	router.DELETE("/user", func(c *gin.Context) { DeleteUser(config, c) })
 
 	if runLocal {
-		log.Fatal(r.Run(config.HTTPAddr))
+		log.Fatal(router.Run(config.HTTPAddr))
 	} else {
-		log.Fatal(autotls.Run(r, "example1.com", "example2.com")) // TODO: real hostname .. https://github.com/kmulvey/trashmap/issues/1
+		log.Fatal(autotls.Run(router, config.HTTPAddr))
 	}
 }
