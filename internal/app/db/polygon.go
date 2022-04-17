d98e9679e887f7e9fa8932a0a31afabb5e6922a5
Kevin Mulvey <kmulvey@linux.com>
2022-04-17 10:09:16 -0600

    GetPolygonsWithinArea

--- internal/app/db/polygon.go
+++ internal/app/db/polygon.go
@@ -10,3 +10,9 @@ func InsertArea(db *sql.DB, userID int, points string) error {
 	var _, err = db.Exec(updateStmt, userID, points)
 	return err
 }
+
+func GetPolygonsWithinArea(db *sql.DB, points string) error {
+	updateStmt := `INSERT INTO areas(user_id, polygon) VALUES($1, ST_GeometryFromText('POLYGON(($2))))`
+	var _, err = db.Exec(updateStmt, points)
+	return err
+}
--- internal/app/polygon/polygon.go
+++ internal/app/polygon/polygon.go
@@ -9,6 +9,23 @@ import (
 	"github.com/kmulvey/trashmap/internal/app/db"
 )
 
+func GetPolygonsWithinArea(config *config.Config, polygonStr string) error {
+	var polygonArr = strings.Split(polygonStr, ",")
+
+	// first check that we got enough data
+	if len(polygonArr) == 8 {
+		return errors.New("gps points are malformed (lenght must be 8)")
+	}
+
+	// chech that the things in between the commas are actually floats
+	for _, point := range polygonArr {
+		if _, err := strconv.ParseFloat(point, 64); err != nil {
+			return err
+		}
+	}
+
+	return db.InsertArea(config.DBConn, userID, polygonStr)
+}
 func SavePolygon(config *config.Config, userID int, polygonStr string) error {
 	var polygonArr = strings.Split(polygonStr, ",")
 
